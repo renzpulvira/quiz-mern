e70774cb247a919fc39d56e3a962212ee5a1fc6a
renzpulvira <calypsogaming098two@gmail.com>
2021-03-11 00:20:26 +0800

    added TImer

--- src/views/Quiz.jsx
+++ src/views/Quiz.jsx
@@ -5,26 +5,49 @@ export default function Quiz() {
   const [isFinished, setIsFinished] = useState(false);
   const [minutes, setMinutes] = useState(1);
   const [seconds, setSeconds] = useState(3);
+  const [isStarted, setIsStarted] = useState(false);
+
+  const callSetFinished = () => {
+    setIsFinished(true);
+  };
+
+  const callSetMinutesSeconds = (min, sec) => {
+    setMinutes(min);
+    setSeconds(sec);
+  };
+
+  const callSetSeconds = (sec) => {
+    setSeconds(sec);
+  };
+
+  let myTimer = null;
 
   useEffect(() => {
-    let myTimer = setInterval(() => {
-      if (seconds > 0) {
-        setSeconds(seconds - 1);
-      }
-      if (seconds === 0) {
-        if (minutes === 0) {
-          setIsFinished(true);
-          clearInterval(myTimer);
-        } else {
-          setMinutes(minutes - 1);
-          setSeconds(3);
+    if (isStarted) {
+      myTimer = setInterval(() => {
+        if (seconds > 0) {
+          callSetSeconds(seconds - 1);
         }
-      }
-    }, 1000);
+        if (seconds === 0) {
+          if (minutes === 0) {
+            callSetFinished();
+            clearInterval(myTimer);
+          } else {
+            // setMinutes(minutes - 1);
+            // setSeconds(3);
+            callSetMinutesSeconds(minutes - 1, 3);
+          }
+        }
+      }, 1000);
 
-    return () => {
+      return () => {
+        clearInterval(myTimer);
+      };
+    }
+    if (!isStarted) {
       clearInterval(myTimer);
-    };
+      myTimer = null;
+    }
   });
 
   return (
@@ -32,6 +55,9 @@ export default function Quiz() {
       <p>Timer Status: {isFinished ? "Ended" : "In Progress"}</p>
       <p>{minutes}</p>
       <p>{seconds}</p>
+
+      <button onClick={() => setIsStarted(!isStarted)}>TOGGLE ON/OFF</button>
+      <p>{isStarted ? "ON" : "OFF"}</p>
     </div>
   );
 }
